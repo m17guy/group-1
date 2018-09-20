9c5c3e0d9b964081bb1c3f15184189602fbd6732
m17guy <m1734guy@gmail.com>
2018-09-20 17:19:30 +0300

    pre

---
+++ the project/the project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace the_project
+{
+    //                                                                           *************   be creative   *************
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
